059e9bef69bb3061bd1dd88527093912f72ab62c
Devpratap Singh <devprataprathore346@gmail.com>
2026-06-21 13:38:43 +0530

    Add Medium Number [A] from Codeforces

---
+++ 1760/A - Medium Number/solution.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+ 
+    while(t--){
+        int a, b, c;
+        cin >> a >> b >> c;
+        int arr[] = {a, b, c};
+ 
+        sort(arr, arr+3);
+ 
+        cout << arr[1] << endl;
+        
+    }
+ 
+    return 0;
+}
