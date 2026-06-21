f02969cdf44e369d3c73230fcbdd7cdd4b46391b
Devpratap Singh <devprataprathore346@gmail.com>
2026-06-21 13:35:34 +0530

    Add Elephant [A] from Codeforces

---
+++ 617/A - Elephant/solution.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int x;
+    cin >> x;
+    int steps = 0;
+ 
+    while(x>0){
+        if(x>=5){
+            x-=5;
+            steps++;
+        }else if(x>=4){
+            x-=4;
+            steps++;
+        }else if(x>=3){
+            x-=3;
+            steps++;
+        }else if(x>=2){
+            x-=2;
+            steps++;
+        }else{
+            x--;
+            steps++;
+        }
+        }
+        
+        cout << steps << endl;
+        return 0;
+}
+        
