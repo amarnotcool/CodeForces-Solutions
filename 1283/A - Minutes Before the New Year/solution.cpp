ea2ec93821177f6b018b601b5d096d419fb80f95
Devpratap Singh <devprataprathore346@gmail.com>
2026-06-21 13:49:13 +0530

    Add Minutes Before the New Year [A] from Codeforces

---
+++ 1283/A - Minutes Before the New Year/solution.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+    
+void fast_io() {
+std::ios_base::sync_with_stdio(false);
+std::cin.tie(NULL);
+std::cout.tie(NULL);
+}
+    
+signed main(){
+    fast_io();
+    
+    int t;
+    cin >> t;
+ 
+    while(t--){
+        int h, m;
+        cin >> h >> m;
+ 
+        int mins = 0;
+ 
+        mins += (60-m);
+        h++;
+ 
+        mins += (24-h)*60;
+ 
+        cout << mins << endl;
+    }
+    
+    return 0;
+}
