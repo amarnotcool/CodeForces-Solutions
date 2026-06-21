f2ad24bea4cef9cd7ddbf09c2aac74c39513756c
Devpratap Singh <devprataprathore346@gmail.com>
2026-06-21 13:46:44 +0530

    Add Bogosort [B] from Codeforces

---
+++ 1312/B - Bogosort/solution.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+    
+void fast_io() {
+std::ios_base::sync_with_stdio(false);
+std::cin.tie(NULL);
+std::cout.tie(NULL);
+}
+    
+signed main(){
+    fast_io();
+    
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int n;
+        cin >> n;
+        vector <int> arr(n);
+ 
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+ 
+        sort(arr.rbegin(), arr.rend());
+ 
+        for(auto a:arr){
+            cout << a << " ";
+        }
+ 
+        cout << endl;
+    }
+    
+    return 0;
+}
