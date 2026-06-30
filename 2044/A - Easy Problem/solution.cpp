3fe911ab704827faec269ba2f6a057ac1b099838
Devpratap Singh <devprataprathore346@gmail.com>
2026-06-30 22:52:53 +0530

    Add Easy Problem [A] from Codeforces

---
+++ 2044/A - Easy Problem/solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+    
+void fast_io() {
+std::ios_base::sync_with_stdio(false);
+std::cin.tie(NULL);
+std::cout.tie(NULL);
+}
+    
+signed main(){
+    fast_io();
+    
+    int t;
+    cin >> t;
+ 
+    while(t--){
+        int n;
+        cin >> n;
+ 
+        cout << n-1 << endl;
+    }
+    
+    return 0;
+}
